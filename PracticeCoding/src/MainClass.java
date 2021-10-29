a5bd99ef9a9cba76516bc8894fb48cb9eb102ee6
upreti.himanshu <upretihimanshu16@gmail.com>
2021-10-29 12:05:32 +0530

    array Duplicate remove Method

--- PracticeCoding/src/MainClass.java
+++ PracticeCoding/src/MainClass.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.upreti.practice.arrayrelated.ArrayRelated;
 import com.upreti.practice.stringrelated.StringRelatedMethods;
 
 public class MainClass {
@@ -7,6 +12,18 @@ public class MainClass {
 		StringRelatedMethods stRelatedMethod = new StringRelatedMethods();
 		String outsides = stRelatedMethod.getIndexOfDuplicateCharacterInGivenString("I am Himanshu");
 		System.out.println("outside = "+outsides);
+		
+		ArrayRelated arrRelated = new ArrayRelated();
+		List<Object> returnList = new ArrayList<Object>();
+		returnList.add("Jai");
+		returnList.add("Himanshu");
+		returnList.add("Upreti");
+		returnList.add("Jai");
+		returnList.add("OM");
+		returnList.add("Jai");
+		
+		System.out.println("List= "+arrRelated.removeDuplicate(returnList).toString());
+		
 		
 	}
 }
--- PracticeCoding/src/com/upreti/practice/arrayrelated/ArrayRelated.java
+++ PracticeCoding/src/com/upreti/practice/arrayrelated/ArrayRelated.java
@@ -1,5 +1,16 @@
 package com.upreti.practice.arrayrelated;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ArrayRelated {
 
+	public List<Object> removeDuplicate(List<Object> arrayList) {
+		List<Object> returnList = new ArrayList<Object>();
+		Set<Object> removeDuplicateString = new HashSet<Object>(arrayList);
+		returnList.add(removeDuplicateString);
+		return returnList;
+	}
 }
