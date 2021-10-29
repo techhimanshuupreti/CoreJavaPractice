1a1627cfad8fd23d7d3c027fcdc71c0dfd026aff
upreti.himanshu <upretihimanshu16@gmail.com>
2021-10-29 15:41:42 +0530

    find day name from date

--- PracticeCoding/src/com/upreti/practice/stringrelated/StringRelatedMethods.java
+++ PracticeCoding/src/com/upreti/practice/stringrelated/StringRelatedMethods.java
@@ -1,5 +1,6 @@
 package com.upreti.practice.stringrelated;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -140,5 +141,12 @@ public class StringRelatedMethods {
 			 }
 		   }
 	   return returnStr.replaceFirst(" ", "");
+   }
+   
+   public String findDay(int month, int day, int year) {
+   	Calendar calendar = Calendar.getInstance();
+   	calendar.set(year, month-1, day);
+   	String[] days = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+   	return days[calendar.get(Calendar.DAY_OF_WEEK) - 1].toUpperCase();
    }
 }
